fc448a7f63e386c5cba4672549ad491e0d9f34ac
nithish0302 <nithi2005palani@gmail.com>
2025-02-26 09:59:32 +0530

    File modified

--- bikerental/src/App.jsx
+++ bikerental/src/App.jsx
@@ -3,8 +3,6 @@ import "./App.css";
 import "./Components";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return <></>;
 }
 
---
+++ bikerental/src/Components/CustomerFrontPage.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import "./App.css";
+import "./Components";
+
+export default function CustomerFrontPage() {
+  return <></>;
+}
